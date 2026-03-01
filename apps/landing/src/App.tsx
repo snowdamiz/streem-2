d954d45c1082bdff53002daa33950095cec54923
snowdamiz <yurlovandrew@gmail.com>
2026-03-01 16:51:19 -0500

    feat(13-01): wire BenchmarkChart into landing App.tsx
    - Import BenchmarkChart from './components/BenchmarkChart'
    - Place <BenchmarkChart /> between <TickerDemo /> and <CodeSample />
    - New section order: Nav > Hero > StatsBar > Features > TickerDemo > BenchmarkChart > CodeSample > InstallCta > Footer

--- apps/landing/src/App.tsx
+++ apps/landing/src/App.tsx
@@ -1,17 +1,25 @@
+import { Nav } from './components/Nav'
 import { Hero } from './components/Hero'
-import { TickerDemo } from './components/TickerDemo'
+import { StatsBar } from './components/StatsBar'
 import { Features } from './components/Features'
+import { TickerDemo } from './components/TickerDemo'
+import { BenchmarkChart } from './components/BenchmarkChart'
 import { CodeSample } from './components/CodeSample'
 import { InstallCta } from './components/InstallCta'
+import { Footer } from './components/Footer'
 
 export function App() {
   return (
     <div>
+      <Nav />
       <Hero />
-      <TickerDemo />
+      <StatsBar />
       <Features />
+      <TickerDemo />
+      <BenchmarkChart />
       <CodeSample />
       <InstallCta />
+      <Footer />
     </div>
   )
 }
